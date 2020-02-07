9a848f824fdc894a9469d61439305c676d096b7f
Marco Walz <walz.marcoo@gmail.com>
2020-02-07 19:18:12 +0100

    use URLs defined in system env

--- src/test/java/com/kryptokrauts/codegen/maven/BaseTest.java
+++ src/test/java/com/kryptokrauts/codegen/maven/BaseTest.java
@@ -1,12 +1,5 @@
 package com.kryptokrauts.codegen.maven;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.apache.maven.plugin.MojoExecutionException;
-import org.junit.jupiter.api.BeforeAll;
-
 import com.google.common.collect.ImmutableSet;
 import com.google.common.reflect.ClassPath;
 import com.google.common.reflect.ClassPath.ClassInfo;
@@ -17,95 +10,97 @@ import com.kryptokrauts.aeternity.sdk.service.aeternity.AeternityServiceConfigur
 import com.kryptokrauts.aeternity.sdk.service.keypair.KeyPairService;
 import com.kryptokrauts.aeternity.sdk.service.keypair.KeyPairServiceFactory;
 import com.kryptokrauts.codegen.CodegenConfiguration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.junit.jupiter.api.BeforeAll;
 
 public abstract class BaseTest {
 
-	protected static CodegenConfiguration config;
+  protected static CodegenConfiguration config;
 
-	protected static final String AETERNITY_BASE_URL = "AETERNITY_BASE_URL";
+  protected static final String AETERNITY_BASE_URL = "AETERNITY_BASE_URL";
 
-	protected static final String COMPILER_BASE_URL = "COMPILER_BASE_URL";
+  protected static final String COMPILER_BASE_URL = "COMPILER_BASE_URL";
 
-	protected static final String targetPath = "target/generated-sources/contraect";
+  protected static final String targetPath = "target/generated-sources/contraect";
 
-	protected static final String targetPackage = "com.kryptokrauts.contraect.generated";
+  protected static final String targetPackage = "com.kryptokrauts.contraect.generated";
 
-	protected static AeternityServiceConfiguration aeternityServiceConfig;
+  protected static AeternityServiceConfiguration aeternityServiceConfig;
 
-	@BeforeAll
-	public static void initConfig() throws MojoExecutionException {
-		ABIJsonDescription abiJsonDescription = new ABIJsonDescription();
-		config = CodegenConfiguration.builder()
-				.compilerBaseUrl(getCompilerBaseUrl()).targetPath(targetPath)
-				.targetPackage(targetPackage)
-				.datatypePackage(targetPackage + ".datatypes").numTrials(60)
-				.initFunctionName(abiJsonDescription.getInitFunctionName())
-				.abiJSONFunctionArgumentElement(
-						abiJsonDescription.getAbiJSONFunctionArgumentElement())
-				.abiJSONFunctionArgumentNameElement(abiJsonDescription
-						.getAbiJSONFunctionArgumentNameElement())
-				.abiJSONFunctionArgumentTypeElement(abiJsonDescription
-						.getAbiJSONFunctionArgumentTypeElement())
-				.abiJSONFunctionsElement(
-						abiJsonDescription.getAbiJSONFunctionsElement())
-				.abiJSONFunctionsNameElement(
-						abiJsonDescription.getAbiJSONFunctionsNameElement())
-				.abiJSONFunctionsReturnTypeElement(abiJsonDescription
-						.getAbiJSONFunctionsReturnTypeElement())
-				.abiJSONFunctionStatefulElement(
-						abiJsonDescription.getAbiJSONFunctionStatefulElement())
-				.abiJSONNameElement(abiJsonDescription.getAbiJSONNameElement())
-				.abiJSONRootElement(abiJsonDescription.getAbiJSONRootElement())
-				.abiJSONTypesElement(
-						abiJsonDescription.getAbiJSONTypesElement())
-				.abiJSONTypesNameElement(
-						abiJsonDescription.getAbiJSONTypesNameElement())
-				.abiJSONTypesTypedefElement(
-						abiJsonDescription.getAbiJSONTypesTypedefElement())
-				.resultAbortKey(abiJsonDescription.getResultAbortKey()).build();
+  @BeforeAll
+  public static void initConfig() throws MojoExecutionException {
+    ABIJsonDescription abiJsonDescription = new ABIJsonDescription();
+    config =
+        CodegenConfiguration.builder()
+            .compilerBaseUrl(getCompilerBaseUrl())
+            .targetPath(targetPath)
+            .targetPackage(targetPackage)
+            .datatypePackage(targetPackage + ".datatypes")
+            .numTrials(60)
+            .initFunctionName(abiJsonDescription.getInitFunctionName())
+            .abiJSONFunctionArgumentElement(abiJsonDescription.getAbiJSONFunctionArgumentElement())
+            .abiJSONFunctionArgumentNameElement(
+                abiJsonDescription.getAbiJSONFunctionArgumentNameElement())
+            .abiJSONFunctionArgumentTypeElement(
+                abiJsonDescription.getAbiJSONFunctionArgumentTypeElement())
+            .abiJSONFunctionsElement(abiJsonDescription.getAbiJSONFunctionsElement())
+            .abiJSONFunctionsNameElement(abiJsonDescription.getAbiJSONFunctionsNameElement())
+            .abiJSONFunctionsReturnTypeElement(
+                abiJsonDescription.getAbiJSONFunctionsReturnTypeElement())
+            .abiJSONFunctionStatefulElement(abiJsonDescription.getAbiJSONFunctionStatefulElement())
+            .abiJSONNameElement(abiJsonDescription.getAbiJSONNameElement())
+            .abiJSONRootElement(abiJsonDescription.getAbiJSONRootElement())
+            .abiJSONTypesElement(abiJsonDescription.getAbiJSONTypesElement())
+            .abiJSONTypesNameElement(abiJsonDescription.getAbiJSONTypesNameElement())
+            .abiJSONTypesTypedefElement(abiJsonDescription.getAbiJSONTypesTypedefElement())
+            .resultAbortKey(abiJsonDescription.getResultAbortKey())
+            .build();
 
-		KeyPairService keyPairService = new KeyPairServiceFactory()
-				.getService();
+    KeyPairService keyPairService = new KeyPairServiceFactory().getService();
 
-		BaseKeyPair baseKeyPair = keyPairService.generateBaseKeyPairFromSecret(
-				"79816BBF860B95600DDFABF9D81FEE81BDB30BE823B17D80B9E48BE0A7015ADF");
+    BaseKeyPair baseKeyPair =
+        keyPairService.generateBaseKeyPairFromSecret(
+            "79816BBF860B95600DDFABF9D81FEE81BDB30BE823B17D80B9E48BE0A7015ADF");
 
-		aeternityServiceConfig = AeternityServiceConfiguration.configure()
-				.compilerBaseUrl("http://compiler.aelocal:3080")
-				.baseUrl("http://aelocal").network(Network.DEVNET)
-				.baseKeyPair(baseKeyPair).targetVM(VirtualMachine.FATE)
-				.compile();
-	}
+    aeternityServiceConfig =
+        AeternityServiceConfiguration.configure()
+            .compilerBaseUrl(getCompilerBaseUrl())
+            .baseUrl(getNodeBaseUrl())
+            .network(Network.DEVNET)
+            .baseKeyPair(baseKeyPair)
+            .targetVM(VirtualMachine.FATE)
+            .compile();
+  }
 
-	protected static String getNodeBaseUrl() throws MojoExecutionException {
-		String nodeBaseUrl = System.getenv(AETERNITY_BASE_URL);
-		if (nodeBaseUrl == null) {
-			throw new MojoExecutionException(
-					"ENV variable missing: AETERNITY_BASE_URL");
-		}
-		return nodeBaseUrl;
-	}
+  protected static String getNodeBaseUrl() throws MojoExecutionException {
+    String nodeBaseUrl = System.getenv(AETERNITY_BASE_URL);
+    if (nodeBaseUrl == null) {
+      throw new MojoExecutionException("ENV variable missing: AETERNITY_BASE_URL");
+    }
+    return nodeBaseUrl;
+  }
 
-	protected static String getCompilerBaseUrl() throws MojoExecutionException {
-		String compilerBaseUrl = System.getenv(COMPILER_BASE_URL);
-		if (compilerBaseUrl == null) {
-			throw new MojoExecutionException(
-					"ENV variable missing: COMPILER_BASE_URL");
-		}
-		return compilerBaseUrl;
-	}
+  protected static String getCompilerBaseUrl() throws MojoExecutionException {
+    String compilerBaseUrl = System.getenv(COMPILER_BASE_URL);
+    if (compilerBaseUrl == null) {
+      throw new MojoExecutionException("ENV variable missing: COMPILER_BASE_URL");
+    }
+    return compilerBaseUrl;
+  }
 
-	protected Map<String, ClassInfo> getCustomDatatypes() {
-		try {
-			ImmutableSet<ClassInfo> cls = ClassPath
-					.from(Thread.currentThread().getContextClassLoader())
-					.getTopLevelClasses();
-			return cls.stream()
-					.filter(c -> c.getPackageName().startsWith(targetPackage))
-					.collect(Collectors.toMap(c -> c.getSimpleName(), c -> c));
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new HashMap<>();
-		}
-	}
+  protected Map<String, ClassInfo> getCustomDatatypes() {
+    try {
+      ImmutableSet<ClassInfo> cls =
+          ClassPath.from(Thread.currentThread().getContextClassLoader()).getTopLevelClasses();
+      return cls.stream()
+          .filter(c -> c.getPackageName().startsWith(targetPackage))
+          .collect(Collectors.toMap(c -> c.getSimpleName(), c -> c));
+    } catch (Exception e) {
+      e.printStackTrace();
+      return new HashMap<>();
+    }
+  }
 }
