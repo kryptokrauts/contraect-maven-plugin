a5101bca68213801bc0dbf7d6b424c38b596cadc
Michel Meier <dna.fan@gmx.de>
2020-02-19 23:14:58 +0100

    cleanup of tests

--- src/test/java/com/kryptokrauts/codegen/maven/ContraectCodegenMojoTest.java
+++ src/test/java/com/kryptokrauts/codegen/maven/ContraectCodegenMojoTest.java
@@ -1,21 +1,24 @@
 package com.kryptokrauts.codegen.maven;
 
-import com.kryptokrauts.codegen.ContraectGenerator;
 import java.io.File;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.junit.jupiter.api.Test;
 
+import com.kryptokrauts.codegen.ContraectGenerator;
+
 public class ContraectCodegenMojoTest extends BaseTest {
 
-  @Test
-  public void testCompileContraects() throws MojoExecutionException {
+	@Test
+	public void testCompileContraects() throws MojoExecutionException {
 
-    ContraectGenerator generator = new ContraectGenerator(config, abiJsonDescription);
-    generator.generate(new File("src/test/resources/contraects/SophiaTypes.aes").getAbsolutePath());
-
-    generator.generate(
-        new File("src/test/resources/contraects/DatatypeTest.aes").getAbsolutePath());
-    generator.generate(
-        new File("src/test/resources/contraects/CryptoHamster.aes").getAbsolutePath());
-  }
+		ContraectGenerator generator = new ContraectGenerator(config,
+				abiJsonDescription);
+		generator.generate(
+				new File("src/test/resources/contraects/SophiaTypes.aes")
+						.getAbsolutePath());
+		generator.generate(
+				new File("src/test/resources/contraects/CryptoHamster.aes")
+						.getAbsolutePath());
+	}
 }
