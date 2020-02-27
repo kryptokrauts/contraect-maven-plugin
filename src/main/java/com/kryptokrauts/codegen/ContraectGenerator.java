f624089d52b804389ba68e699d86ea32f38b1233
Michel Meier <dna.fan@gmx.de>
2020-02-27 20:18:20 +0100

    updated javadoc

--- src/main/java/com/kryptokrauts/codegen/ContraectGenerator.java
+++ src/main/java/com/kryptokrauts/codegen/ContraectGenerator.java
@@ -197,9 +197,11 @@ public class ContraectGenerator {
               .addMethods(buildContractPrivateMethods())
               .addMethod(buildConstructor())
               .addMethods(this.buildContractMethods(abiJson))
+              .addJavadoc(LICENSE_HEADER)
+              .addJavadoc(KRYPTOKRAUTS)
+              .addJavadoc(LICENSE)
               .addJavadoc(GENERATED)
               .addJavadoc(SUPPORT)
-              .addJavadoc(KRYPTOKRAUTS)
               .build();
 
       /** add default deploy method if no explicit method defined in contract */
@@ -1057,12 +1059,27 @@ public class ContraectGenerator {
       "If you like this project we would appreciate your support.\r\nYou can find multiple ways to support us here\r\nhttps://kryptokrauts.com/support\r\n";
 
   private String KRYPTOKRAUTS =
-      " _                         _          _                        _        \r\n"
-          + "| |                       | |        | |                      | |       \r\n"
-          + "| | __ _ __  _   _  _ __  | |_  ___  | | __ _ __  __ _  _   _ | |_  ___ \r\n"
-          + "| |/ /| '__|| | | || '_ \\ | __|/ _ \\ | |/ /| '__|/ _` || | | || __|/ __|\r\n"
-          + "|   < | |   | |_| || |_) || |_| (_) ||   < | |  | (_| || |_| || |_ \\__ \\\r\n"
-          + "|_|\\_\\|_|    \\__, || .__/  \\__|\\___/ |_|\\_\\|_|   \\__,_| \\__,_| \\__||___/\r\n"
-          + "              __/ || |                                                  \r\n"
-          + "             |___/ |_|        \r\n";
+      " _                     _        _                    _                            \r\n"
+          + "| |                   | |      | |                  | |                           \r\n"
+          + "| | ___ __ _   _ _ __ | |_ ___ | | ___ __ __ _ _   _| |_ ___   ___ ___  _ __ ___  \r\n"
+          + "| |/ / '__| | | | '_ \\| __/ _ \\| |/ / '__/ _` | | | | __/ __| / __/ _ \\| '_ ` _ \\ \r\n"
+          + "|   <| |  | |_| | |_) | || (_) |   <| | | (_| | |_| | |_\\__ \\| (_| (_) | | | | | |\r\n"
+          + "|_|\\_\\_|   \\__, | .__/ \\__\\___/|_|\\_\\_|  \\__,_|\\__,_|\\__|___(_)___\\___/|_| |_| |_|\r\n"
+          + "            __/ | |                                                               \r\n"
+          + "           |___/|_|\r\n";
+
+  private String LICENSE_HEADER = "\r\nISC License\r\n" + "\r\nCopyright (c) 2020\r\n";
+
+  private String LICENSE =
+      "\r\nPermission to use, copy, modify, and/or distribute this software for any\r\n"
+          + "purpose with or without fee is hereby granted, provided that the above\r\n"
+          + "copyright notice and this permission notice appear in all copies.\r\n"
+          + "\r\n"
+          + "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH\r\n"
+          + "REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY\r\n"
+          + "AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,\r\n"
+          + "INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM\r\n"
+          + "LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE\r\n"
+          + "OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR\r\n"
+          + "PERFORMANCE OF THIS SOFTWARE.\r\n";
 }
